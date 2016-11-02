f600a4e00fe98415c5071f4a51b4ffabd828ea40
oliviaridgley <lilyr94@comcast.net>
2016-11-01 20:04:20 -0600

    Added CNAME

--- js/script.js
+++ js/script.js
@@ -3,4 +3,14 @@ $( document ).ready(function() {
     console.log( "ready!" );
     $(".dropdown-button").dropdown();
     $(".button-collapse").sideNav();
+
+
+// var url =
+// "http://www.colourlovers.com/api/palettes?format=json&hex=990000&numResults=3"
+//
+// $.getJSON(url,function(response) {
+//   console.log(response)
+// })
+
+
 });
